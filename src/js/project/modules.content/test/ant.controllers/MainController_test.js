d564f846569232631df26185c2e3bfb4d2719656
Hamid Behnam <hamid.behnam@gmail.com>
2016-08-19 11:45:52 -0700

    adding test frameworks

---
+++ src/js/project/modules.content/test/ant.controllers/MainController_test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hamidbehnam on 8/19/16.
+ */
+
+describe("MainController", function () {
+   it("should return 5", function () {
+       var testField = 5;
+       expect(testField).toBe(5);
+   })
+});
