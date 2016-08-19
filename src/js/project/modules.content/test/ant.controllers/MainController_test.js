13650c62b75dea212ade400a4e0a48e33235f7b6
Hamid Behnam <hamid.behnam@gmail.com>
2016-08-19 14:10:49 -0700

    changing the test spec file

--- src/js/project/modules.content/test/ant.controllers/MainController_test.js
+++ src/js/project/modules.content/test/ant.controllers/MainController_test.js
@@ -3,8 +3,121 @@
  */
 
 describe("MainController", function () {
-   it("should return 5", function () {
-       var testField = 5;
-       expect(testField).toBe(5);
-   })
+    it("should return 5", function () {
+        var testField = 5;
+        expect(testField).toBe(5);
+    });
+
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
+    it("should return 44", function () {
+        var something = 44;
+        expect(something).toBe(44);
+    });
 });
